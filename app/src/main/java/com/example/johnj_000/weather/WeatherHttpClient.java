6d039e17ebcefee9b8bae1f0347c5aa7322a722c
hildyale <hildyale@hotmail.com>
2015-11-25 16:09:48 -0500

    imagen_epa

--- app/src/main/java/com/example/johnj_000/weather/WeatherHttpClient.java
+++ app/src/main/java/com/example/johnj_000/weather/WeatherHttpClient.java
@@ -59,7 +59,7 @@ public class WeatherHttpClient {
         HttpURLConnection con = null ;
         InputStream is = null;
         try {
-            con = (HttpURLConnection) ( new URL(IMG_URL + code)).openConnection();
+            con = (HttpURLConnection) ( new URL(IMG_URL + code+".png")).openConnection();
             con.setRequestMethod("GET");
             con.setDoInput(true);
             con.setDoOutput(true);
